c6e33123556b27f0011786bdcf1fecaedb2ad40b
MarcosLabra <mlabra.dev@gmail.com>
2022-04-25 13:45:33 -0300

    first commit

---
+++ db/mensajes.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+class Mensajes {
+  constructor(fileName) {
+    this.fileName = fileName;
+  }
+
+  async save(msj) {
+    try {
+      if (fs.existsSync(this.fileName)) {
+        const mensajes = JSON.parse(await fs.promises.readFile(this.fileName, 'utf-8'));
+        mensajes.push(msj);
+        fs.writeFileSync(this.fileName, JSON.stringify(mensajes));
+      } else {
+        const mensajes = [];
+        mensajes.push(msj);
+        fs.writeFileSync(this.fileName, JSON.stringify(mensajes));
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  getAll() {
+    try {
+      const contenido = fs.readFileSync(this.fileName, 'utf-8');
+      return JSON.parse(contenido);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = Mensajes;
